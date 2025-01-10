b86d0b4288db758823b9d89fa69c3bd88e7751f7
Aranza Rodriguez <rodriguez.aranza.9801@gmail.com>
2025-01-09 23:16:50 -0600

    deleted user JSON request function

--- api.js
+++ api.js
@@ -15,44 +15,44 @@ client.connect()
 //  Sets up an HTTP GET endpoint at /users
 //  When a GET request is made to /users, the callback function is triggered.
 //  DELETE LATER: Gets ALL users 
-app.get('/users', (req, res)=> {
-    //  Once the query completes, the callback function will execute 
-    client.query('Select * from users', (err, result)=> {
-        if (!err) {
-            //  Sends data back to the client 
-            res.send(result.rows)
-        }
-    })
-    client.end
-})
+// app.get('/users', (req, res)=> {
+//     //  Once the query completes, the callback function will execute 
+//     client.query('Select * from users', (err, result)=> {
+//         if (!err) {
+//             //  Sends data back to the client 
+//             res.send(result.rows)
+//         }
+//     })
+//     client.end
+// })
 
 //  DELETE LATER: Gets a specific user
-app.get('/users/:id', (req, res)=> {
-    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
-        if (!err) {
-            res.send(result.rows)
-        }
-    })
-    client.end
-})
+// app.get('/users/:id', (req, res)=> {
+//     client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
+//         if (!err) {
+//             res.send(result.rows)
+//         }
+//     })
+//     client.end
+// })
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 //  DELETE LATER: Adds new user 
-app.post('/users', (req, res)=> {
-    const user = req.body
-    let insertQuery = `insert into users(id, firstname, lastname) 
-                       values(${user.id}, '${user.firstname}', '${user.lastname}')`
+// app.post('/users', (req, res)=> {
+//     const user = req.body
+//     let insertQuery = `insert into users(id, firstname, lastname) 
+//                        values(${user.id}, '${user.firstname}', '${user.lastname}')`
 
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
-            res.send('Insertion was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
-})
+//     client.query(insertQuery, (err, result)=>{
+//         if(!err){
+//             res.send('Insertion was successful')
+//         }
+//         else{ console.log(err.message) }
+//     })
+//     client.end;
+// })
 
 //  Detects whether the endpoint was successfull 
 app.post('/crawl', async (req, res)=> {
@@ -85,34 +85,34 @@ app.post('/crawl', async (req, res)=> {
 
 
 //  DELETE LATER: Updates user 
-app.put('/users/:id', (req, res)=> {
-    let user = req.body
-    let updatedQuery = `update users
-                        set firstname = '${user.firstname}',
-                        lastname = '${user.lastname}'
-                        where id = ${user.id}`
+// app.put('/users/:id', (req, res)=> {
+//     let user = req.body
+//     let updatedQuery = `update users
+//                         set firstname = '${user.firstname}',
+//                         lastname = '${user.lastname}'
+//                         where id = ${user.id}`
 
-    client.query(updatedQuery, (err, result)=> {
-        if (!err) {
-            res.send('Update was successful!')
-        } else {
-            console.log(err.message)
-        }
-    })
-    client.end
-})
+//     client.query(updatedQuery, (err, result)=> {
+//         if (!err) {
+//             res.send('Update was successful!')
+//         } else {
+//             console.log(err.message)
+//         }
+//     })
+//     client.end
+// })
 
 //  DELETE LATER: 
-app.delete('/users/:id', (req, res)=> {
-    let insertQuery = `delete from users where id=${req.params.id}`
+// app.delete('/users/:id', (req, res)=> {
+//     let insertQuery = `delete from users where id=${req.params.id}`
 
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
-            res.send('Deletion was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
-})
+//     client.query(insertQuery, (err, result)=>{
+//         if(!err){
+//             res.send('Deletion was successful')
+//         }
+//         else{ console.log(err.message) }
+//     })
+//     client.end;
+// })
 
 // client.connect()
