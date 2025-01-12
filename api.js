8d8b19024ff564f847280b0d53cf5046bdd6a3be
Aranza Rodriguez <rodriguez.aranza.9801@gmail.com>
2025-01-12 02:00:20 -0600

    POST request has been fully implemented to update postgres

--- api.js
+++ api.js
@@ -5,6 +5,44 @@ const { returnJSONReport } = require(`./report.js`)
 
 const app = express()   //  Creates the server 
 
+const queries = {
+
+    insertWebpage: `
+        INSERT INTO webpages(
+	        request_id, sub_url, root_url)
+	        VALUES ($1, $2::text, $3);`,
+
+    insertExternalUrls: `
+        INSERT INTO external_urls(
+            request_id, base_url, external_urls, root_url)
+            VALUES ($1, $2::text, $3::text[], $4);`,
+
+    insertTags: `
+        INSERT INTO tags(
+            request_id, base_url, title_tags, header_tags, meta_tags, root_url)
+            VALUES ($1, $2::text, $3::text[], $4::text[], $5::text[], $6);`
+}
+
+
+async function insertURLData(client, urlInfo, reqID, rootURL) {
+    
+    const webPageValues = [reqID, urlInfo['base_url'], rootURL]
+    const externalURLsValues = [reqID, urlInfo['base_url'], urlInfo['externalURL'], rootURL]
+    const values = [reqID, urlInfo['base_url'], urlInfo['title_tags'], urlInfo['header_tags'], urlInfo['meta_tags'], rootURL]
+
+    try {
+        // If the query does not fully execute, it will prevent the whole query from being sent 
+        await client.query('BEGIN') 
+        await client.query(queries.insertWebpage, webPageValues)
+        await client.query(queries.insertExternalUrls, externalURLsValues)
+        await client.query(queries.insertTags, values)
+        await client.query('COMMIT')
+        return true
+    } catch (error) {
+        await client.query('ROLLBACK')
+        throw error
+    }
+}
 
 app.listen(3300, ()=> {
     console.log("Server is now listening at port 3300")
@@ -26,6 +64,8 @@ client.connect()
 //     client.end
 // })
 
+
+
 //  DELETE LATER: Gets a specific user
 // app.get('/users/:id', (req, res)=> {
 //     client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
@@ -36,23 +76,16 @@ client.connect()
 //     client.end
 // })
 
+app.get('/crawl/:request_id' (req, res=> {
+    const requestId = req.params.request_id
+
+    
+
+}))
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-//  DELETE LATER: Adds new user 
-// app.post('/users', (req, res)=> {
-//     const user = req.body
-//     let insertQuery = `insert into users(id, firstname, lastname) 
-//                        values(${user.id}, '${user.firstname}', '${user.lastname}')`
-
-//     client.query(insertQuery, (err, result)=>{
-//         if(!err){
-//             res.send('Insertion was successful')
-//         }
-//         else{ console.log(err.message) }
-//     })
-//     client.end;
-// })
 
 //  Detects whether the endpoint was successfull 
 app.post('/crawl', async (req, res)=> {
@@ -61,37 +94,48 @@ app.post('/crawl', async (req, res)=> {
 
         if (!url) {
             console.log("A URL is required")
-            return res.status(400).json({ error: 'URL is required'})
+            return res.status(400).json({ 
+                error: 'URL is required',
+                success: false
+            })
         }
 
         const crawlResults = await crawlWebsite(url)
         const urlsArray = returnJSONReport(crawlResults)
-        rootURL = urlsArray[0]['base_url']
-        randomNum = 1
-
-        //  Insert the info the in query 
-        const insertQuery = `
-           INSERT INTO webpages(
-	        request_id, sub_url, root_url)
-	        VALUES (${randomNum}, '${rootURL}', '${rootURL}')
-        `
-
-        client.query(insertQuery, (err, result)=>{
-            if(!err) {
-                res.send('Insertion was successful')
-            } else {
-                console.log(err.message)
+        const rootURL = urlsArray[0]['base_url']
+        console.log(urlsArray[0])
+        const reqID = Math.floor(Math.random() * 1000000)
+  
+        try {
+            for (const urlInfo of urlsArray) {
+                //Change this from base_url -> sub_url
+                await insertURLData(client, urlInfo, reqID, rootURL)
             }
-        })
-        //  Consider moving this to its own function: 
+
+            return res.status(200).json({
+                message: 'All URLS processed successfully',
+                requestID: reqID,
+                success: true
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                error: error.message,
+                success: false
+            })
+        }
+
         client.end
-        // res.json({ success: true, urls: urlsArray})
 
     } catch (error){
-        res.status(500).json({ error: error.message })
-        console.log(error.message)
+        res.status(500).json({ 
+            error: error.message,
+            success: false
+        })
+        
     }
 })
+
 
 
 //  DELETE LATER: Updates user 
