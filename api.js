cbab4176c8c1c55e8f4c5c66c76da514e20b20b6
Aranza Rodriguez <rodriguez.aranza.9801@gmail.com>
2025-01-19 03:22:50 -0600

    implemented new changes to post request to reflect altered database structure

--- api.js
+++ api.js
@@ -4,38 +4,52 @@ const { crawlWebsite } = require('./main.js')
 const { returnJSONReport } = require(`./report.js`)
 
 const app = express()   //  Creates the server 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.listen(3300, ()=> {
+    console.log("Server is now listening at port 3300")
+})
+
+client.connect()
 
 const queries = {
 
     insertWebpage: `
         INSERT INTO webpages(
-	        request_id, sub_url, root_url)
-	        VALUES ($1, $2::text, $3);`,
+	        request_id, sub_url)
+	        VALUES ($1, $2::text);`,
 
     insertExternalUrls: `
         INSERT INTO external_urls(
-            request_id, base_url, external_urls, root_url)
-            VALUES ($1, $2::text, $3::text[], $4);`,
+            request_id, base_url, external_urls)
+            VALUES ($1, $2::text, $3::text[]);`,
 
     insertTags: `
         INSERT INTO tags(
-            request_id, base_url, title_tags, header_tags, meta_tags, root_url)
-            VALUES ($1, $2::text, $3::text[], $4::text[], $5::text[], $6);`
+            request_id, base_url, title_tags, header_tags, meta_tags)
+            VALUES ($1, $2::text, $3::text[], $4::text[], $5::text[]);`,
+
+    insertRequest: `
+        INSERT INTO requests(
+            request_id, root_url)
+            VALUES ($1, $2::text)
+    `
 }
 
 
-async function insertURLData(client, urlInfo, reqID, rootURL) {
+async function insertURLData(client, urlInfo, reqID) {
     
-    const webPageValues = [reqID, urlInfo['base_url'], rootURL]
-    const externalURLsValues = [reqID, urlInfo['base_url'], urlInfo['externalURL'], rootURL]
-    const values = [reqID, urlInfo['base_url'], urlInfo['title_tags'], urlInfo['header_tags'], urlInfo['meta_tags'], rootURL]
+    const webPageValues = [reqID, urlInfo['base_url']]
+    const externalURLsValues = [reqID, urlInfo['base_url'], urlInfo['externalURL']]
+    const insertTagsValues = [reqID, urlInfo['base_url'], urlInfo['title_tags'], urlInfo['header_tags'], urlInfo['meta_tags']]
 
     try {
         // If the query does not fully execute, it will prevent the whole query from being sent 
         await client.query('BEGIN') 
         await client.query(queries.insertWebpage, webPageValues)
         await client.query(queries.insertExternalUrls, externalURLsValues)
-        await client.query(queries.insertTags, values)
+        await client.query(queries.insertTags, insertTagsValues)
         await client.query('COMMIT')
         return true
     } catch (error) {
@@ -44,11 +58,72 @@ async function insertURLData(client, urlInfo, reqID, rootURL) {
     }
 }
 
-app.listen(3300, ()=> {
-    console.log("Server is now listening at port 3300")
+
+app.get('/crawl/:request_id', (req, res)=> {
+
+    const requestId = req.params.request_id
+
+    if (!requestId) {
+        return res.status(400).json({
+            error: 'request_id is required',
+            success: false
+        })
+    }
+
+    try {
+
+        query = `
+            SELECT * FROM external_urls where request_id=${requestId};
+        `
+        client.query()
+    } catch (err) {
+
+    }
+
 })
 
-client.connect()
+//  Detects whether the endpoint was successfull 
+app.post('/crawl', async (req, res)=> {
+        const { url } = req.body
+
+        if (!url) {
+            return res.status(400).json({ 
+                error: 'URL is required',
+                success: false
+            })
+        }
+
+        const crawlResults = await crawlWebsite(url)
+        const urlsArray = returnJSONReport(crawlResults)
+        const rootURL = urlsArray[0]['base_url']
+        const reqID = Math.floor(Math.random() * 1000000)
+  
+        //  Adds the request into the 
+        const requestValues = [reqID, rootURL]
+        await client.query(queries.insertRequest, requestValues)
+
+        try {
+            for (const urlInfo of urlsArray) {
+                //Change this from base_url -> sub_url
+                await insertURLData(client, urlInfo, reqID)
+            }
+
+            return res.status(200).json({
+                message: 'All URLS processed successfully',
+                requestID: `Your request ID is: ${reqID}. Please save this for future data retrieval of your crawl results.`,
+                success: true
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                error: error.message,
+                success: false
+            })
+        }
+    
+})
+
+
 
 //  Sets up an HTTP GET endpoint at /users
 //  When a GET request is made to /users, the callback function is triggered.
@@ -75,67 +150,6 @@ client.connect()
 //     })
 //     client.end
 // })
-
-app.get('/crawl/:request_id' (req, res=> {
-    const requestId = req.params.request_id
-
-    
-
-}))
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-
-
-//  Detects whether the endpoint was successfull 
-app.post('/crawl', async (req, res)=> {
-    try {
-        const { url } = req.body
-
-        if (!url) {
-            console.log("A URL is required")
-            return res.status(400).json({ 
-                error: 'URL is required',
-                success: false
-            })
-        }
-
-        const crawlResults = await crawlWebsite(url)
-        const urlsArray = returnJSONReport(crawlResults)
-        const rootURL = urlsArray[0]['base_url']
-        console.log(urlsArray[0])
-        const reqID = Math.floor(Math.random() * 1000000)
-  
-        try {
-            for (const urlInfo of urlsArray) {
-                //Change this from base_url -> sub_url
-                await insertURLData(client, urlInfo, reqID, rootURL)
-            }
-
-            return res.status(200).json({
-                message: 'All URLS processed successfully',
-                requestID: reqID,
-                success: true
-            })
-
-        } catch (error) {
-            return res.status(500).json({
-                error: error.message,
-                success: false
-            })
-        }
-
-        client.end
-
-    } catch (error){
-        res.status(500).json({ 
-            error: error.message,
-            success: false
-        })
-        
-    }
-})
-
 
 
 //  DELETE LATER: Updates user 
